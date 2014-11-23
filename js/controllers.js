52353c407c7c0044216103a3a3da6f14b4cf8f54
shtanaka <egustavo4@gmail.com>
2014-11-23 15:41:47 -0300

    mais opcoes

--- js/controllers.js
+++ js/controllers.js
@@ -2,23 +2,32 @@ var app = angular.module('TrabalhoRedes', []);
 
 app.controller('formCtrl', function($scope) {
     $scope.existsServer = 'no';
-    $scope.isShown = function(existsServer) {
+    $scope.isShownApp = function(existsServer) {
         return existsServer === $scope.existsServer;
+    };
+
+    $scope.enableConnection = 'no';
+    $scope.isShownConnectivity = function(enableConnection) {
+        return enableConnection === $scope.enableConnection;
     };
 
     
     $scope.apps = [{name: '', accessNetwork: '', acessInternet: ''}];
     
-    $scope.add = function() {
+    $scope.addApp = function() {
     	$scope.apps.push({name: '', accessNetwork: '', acessInternet: ''});
     }
-    $scope.pop = function() {
-    	$scope.apps.pop({name: '', accessNetwork: '', acessInternet: ''});
+    $scope.popApp = function() {
+    	$scope.apps.pop();
     }
 
+    $scope.vlans = [{name: '', numberOfUsers: 0, internetConnection: false, appConnection: []}];
+    
+    $scope.addApp = function() {
+    	$scope.apps.push({name: '', numberOfUsers: 0, internetConnection: false, appConnection: []});
+    }
+    $scope.popApp = function() {
+    	$scope.apps.pop();
+    }
 
-
-})
-
-
-;
+});
